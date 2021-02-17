237ad8069de33f779fbcd95af9e7907481c3151d
JustinInthaly21 <825134@mystma.org>
2021-02-17 19:59:30 +0000

    planets 1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#WRITE YOUR CODE IN THIS FILE
+def planets(p):
+    a = 0
+    
+    for i in range(0, 8):
+        a = a + i
+        if p == ba:
+            return a
+
+
+
+ba = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+
